c97f7d18cc7270f97f8b0d55272b481c5e478f86
kemoo44 <kemootop22@gmail.com>
2023-03-26 22:31:49 +0200

    c++

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float n,num,res;
+    cout<<"put your counter"<<endl;
+
+    cin>>n;
+    cout<<"put your numbers"<<endl;
+    for(int i=1;i<=n;i++){
+       cin>>num;
+       res+=num;
+
+
+
+
+
+    }cout<<res/n;
+    return 0;
+}
